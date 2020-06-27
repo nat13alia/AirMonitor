5e9dab08b4fe5636113caf3a0ae60ee1d3b6816b
nat13alia <natalia13krawczyk@gmail.com>
2020-06-27 22:27:24 +0200

    Added PM 2,5 and PM 10 indicators

--- AirMonitor/AirMonitor/Views/Controls/ContentWithHeader.xaml.cs
+++ AirMonitor/AirMonitor/Views/Controls/ContentWithHeader.xaml.cs
@@ -10,8 +10,32 @@ using Xamarin.Forms.Xaml;
 namespace AirMonitor.Views.Controls
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ContentWithHeader : ContentView
+    public partial class ContentWithHeader : StackLayout
     {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+            propertyName: nameof(Title),
+            returnType: typeof(string),
+            declaringType: typeof(ContentWithHeader),
+            defaultValue: null);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty ControlContentProperty = BindableProperty.Create(
+            propertyName: nameof(ControlContent),
+            returnType: typeof(View),
+            declaringType: typeof(ContentWithHeader),
+            defaultValue: null);
+
+        public View ControlContent
+        {
+            get { return (View)GetValue(ControlContentProperty); }
+            set { SetValue(ControlContentProperty, value); }
+        }
+
         public ContentWithHeader()
         {
             InitializeComponent();
