fa9b853c3e027a2fd7108ac8391536a3ffc6368c
nat13alia <natalia13krawczyk@gmail.com>
2020-06-28 12:28:59 +0200

    Applied MVVM to HomePage & Added hierarchical Navigation

--- AirMonitor/AirMonitor/ViewModels/HomeViewModel.cs
+++ AirMonitor/AirMonitor/ViewModels/HomeViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace AirMonitor.ViewModels
 {
     class HomeViewModel
     {
+        private readonly INavigation _navigation;
+
+        public HomeViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+        }
+
+        private ICommand _goToDetailsCommans;
+        public ICommand GoToDetailsCommand => _goToDetailsCommans ?? (_goToDetailsCommans = new Command(OnGoToDetails));
+
+        private void OnGoToDetails()
+        {
+            _navigation.PushAsync(new DetailsPage());
+        }
     }
 }
